85979104ed92033e2774cc865517a0b1b950c2f6
Omar Morera <omarurzim@gmail.com>
2024-01-15 14:14:38 -0300

    agregado origin last source

--- src/components/Leads/LeadsSearch.js
+++ src/components/Leads/LeadsSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Col,
   Row,
@@ -14,56 +14,58 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import LeadsTable from './LeadsTable'
 import axios from 'axios'
+
+
 const LeadsSearch = () => {
-  
+
   const thirtyDaysAgo = new Date();
   thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
   const [startDate, setStartDate] = useState(thirtyDaysAgo);
   const [endDate, setEndDate] = useState(new Date());
   const [searchTerm, setSearchTerm] = useState('')
-//PARTE NUEVA ARRANCA
-const [data, setData] = useState([]);
-const [totalResults, setTotalResults] = useState('')
-
-const handleSearch = (/*offsetData*/) => {
-  // Implement your search functionality here
- // let offsetQuery = typeof offsetData === 'number' ? offsetData : "0";
-  const fetchQuery = async (query) => {
-    //console.log(offsetQuery)
-    console.log(query)
-
-    const res = await axios.post('http://localhost:3001/', {
-      query: query,
-    });
-    console.log(res)
-
-    if(res.data[0]){
-    setData(res.data);
-    setTotalResults(res.data[0].suma_contactos)
-    console.log(data)
-    console.log(totalResults)
-    }else{
-      setData([])
-    }
-  };
+  //PARTE NUEVA ARRANCA
+  const [data, setData] = useState([]);
+  const [totalResults, setTotalResults] = useState('')
 
   const originalDate = new Date(startDate);
   const formattedDateStart = `${originalDate.getFullYear()}-${(originalDate.getMonth() + 1).toString().padStart(2, '0')}-${originalDate.getDate().toString().padStart(2, '0')} ${originalDate.getHours().toString().padStart(2, '0')}:${originalDate.getMinutes().toString().padStart(2, '0')}:${originalDate.getSeconds().toString().padStart(2, '0')}`;
 
 
-const originalDateEnd = new Date(endDate);
-const formattedDateEnd = `${originalDateEnd.getFullYear()}-${(originalDateEnd.getMonth() + 1).toString().padStart(2, '0')}-${originalDateEnd.getDate().toString().padStart(2, '0')} ${originalDateEnd.getHours().toString().padStart(2, '0')}:${originalDateEnd.getMinutes().toString().padStart(2, '0')}:${originalDateEnd.getSeconds().toString().padStart(2, '0')}`;
+  const originalDateEnd = new Date(endDate);
+  const formattedDateEnd = `${originalDateEnd.getFullYear()}-${(originalDateEnd.getMonth() + 1).toString().padStart(2, '0')}-${originalDateEnd.getDate().toString().padStart(2, '0')} ${originalDateEnd.getHours().toString().padStart(2, '0')}:${originalDateEnd.getMinutes().toString().padStart(2, '0')}:${originalDateEnd.getSeconds().toString().padStart(2, '0')}`;
 
+  const querySearch = "SELECT c.id AS contact_id,  c.full_name,  c.email,  c.sales_rep, c.phone, c.created_date, CONCAT('$', FORMAT(COALESCE(SUM(p.payment_amount), NULL), 2)) AS sales,  CASE WHEN MIN(c.utm_source) <> 'null' THEN MIN(c.utm_source) END AS utm_source,  CASE WHEN MAX(c.utm_source) <> 'null' THEN MAX(c.utm_source) ELSE CASE WHEN MIN(c.utm_source) <> 'null' THEN MIN(c.utm_source) END END AS lastSource FROM  Contacts c LEFT JOIN  Payments p ON c.id = p.fk_contact WHERE c.created_date BETWEEN '" + formattedDateStart.toString() + "' AND '" + formattedDateEnd.toString() + "'     AND (c.full_name LIKE '%" + searchTerm + "%' OR c.email LIKE '%" + searchTerm + "%' OR c.phone LIKE '%" + searchTerm + "%') GROUP BY  c.email ORDER BY c.created_date DESC"
 
-  fetchQuery("SELECT c.id AS contact_id,  c.full_name,  c.email,  c.sales_rep, c.phone, c.created_date, CONCAT('$', FORMAT(COALESCE(SUM(p.payment_amount), NULL), 2)) AS sales,  MIN(c.utm_source) AS utm_source,  MAX(c.utm_source) AS utmSource FROM  Contacts c LEFT JOIN  Payments p ON c.id = p.fk_contact WHERE c.created_date BETWEEN '" + formattedDateStart.toString() + "' AND '" + formattedDateEnd.toString() + "'     AND (c.full_name LIKE '%" + searchTerm + "%' OR c.email LIKE '%" + searchTerm + "%' OR c.phone LIKE '%" + searchTerm + "%') GROUP BY  c.email ORDER BY c.created_date DESC");
-  console.log('Search term:', searchTerm);
-  console.log('Start Date:', startDate);
-  console.log('End Date:', endDate);
-};
+  const fetchQuery = async (query) => {
+ 
+    const res = await axios.post('http://localhost:3001/', {
+      query: query,
+    });
+    console.log(res)
 
-//PARTE NUEVA TERMINA
+    if (res.data[0]) {
+      setData(res.data);
+      setTotalResults(res.data[0].suma_contactos)
+      console.log(data)
+      console.log(totalResults)
+    } else {
+      setData([])
+    }
+  };
 
+  const handleSearch = () => {
+    fetchQuery(querySearch)
+    console.log('Search term:', searchTerm);
+    console.log('Start Date:', startDate);
+    console.log('End Date:', endDate);
+  };
 
+  //PARTE NUEVA TERMINA
+
+  useEffect(() => {
+    fetchQuery(querySearch)
+
+  }, [])
 
 
   return (
@@ -72,12 +74,12 @@ const formattedDateEnd = `${originalDateEnd.getFullYear()}-${(originalDateEnd.ge
         <Col md="12">
           <Card>
             <CardHeader>
-              
-              
+
+
             </CardHeader>
             <CardBody className="all-icons">
               <Row>
-                
+
                 <Col md="4">
                   <FormGroup>
                     <Label for="startDate">From:</Label>
@@ -106,7 +108,7 @@ const formattedDateEnd = `${originalDateEnd.getFullYear()}-${(originalDateEnd.ge
                 </Col>
               </Row>
               <Row>
-              <Col md="4">
+                <Col md="4">
                   <FormGroup>
                     <Label for="search">Search:</Label>
                     <Input
